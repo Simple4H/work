b0047e3a2f6d80214eb3d5be09788d8dc8a41e00
simple <crescentcxm@gmail.com>
2018-07-28 14:20:09 +0800

    1,整合Redis 2,整合Json 3,整合Cookie 4,项目初始化完成

---
+++ src/main/java/com/simple/common/Const.java
@@ -0,0 +1,11 @@
+package com.simple.common;
+
+/**
+ * Create By S I M P L E On 2018/07/28 14:07:45
+ */
+public class Const {
+
+    public interface RedisTime {
+        int REDIS_CACHE_EXTIME = 60 * 60 * 24 * 3;
+    }
+}
--- src/main/java/com/simple/controller/UserController.java
+++ src/main/java/com/simple/controller/UserController.java
@@ -1,8 +1,21 @@
 package com.simple.controller;
 
+import com.simple.common.Const;
+import com.simple.common.ServerResponse;
+import com.simple.dao.UserMapper;
+import com.simple.pojo.User;
+import com.simple.util.CookieUtil;
+import com.simple.util.JsonUtil;
+import com.simple.util.RedisPoolUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 /**
@@ -12,5 +25,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/user/")
 @Slf4j
 public class UserController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @RequestMapping(value = "test.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse list(HttpServletResponse httpServletResponse, HttpSession session) {
+        User user = userMapper.selectByPrimaryKey(1);
+        CookieUtil.writeLoginToken(httpServletResponse,session.getId());
+        RedisPoolUtil.setEx(session.getId(), Const.RedisTime.REDIS_CACHE_EXTIME, JsonUtil.obj2String(user));
+        return ServerResponse.createBySuccess("success",user);
+
+    }
 
 }
