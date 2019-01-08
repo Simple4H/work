15d0500d54d598e44d5ed94308887c78b718483b
Simple4H <crescentcxm@gmail.com>
2019-01-08 21:34:22 +0800

    测试

---
+++ src/main/java/com/simple/test/Test.java
@@ -0,0 +1,33 @@
+package com.simple.test;
+
+import com.simple.pojo.Data;
+import com.simple.pojo.User;
+import com.simple.util.JsonUtil;
+import com.simple.util.RedisShardedPoolUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Create By S I M P L E On 2019/01/08 21:25:51
+ */
+@Slf4j
+public class Test {
+
+    public static void main(String[] args) {
+        User user1 = new User();
+        user1.setUsername("xiuwen");
+        User user2 = new User();
+        user2.setUsername("simple");
+        List<User> list = new ArrayList<>();
+        list.add(user1);
+        list.add(user2);
+        RedisShardedPoolUtil.set("userList", JsonUtil.obj2String(list));
+        List<User> result = (List<User>) JsonUtil.string2Obj(RedisShardedPoolUtil.get("userList"),List.class);
+        log.info("result:{}",result);
+
+
+    }
+
+}
